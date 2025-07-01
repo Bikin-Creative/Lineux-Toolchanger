44b704be96cbd85bd29015d80a73903ddd8984c3
W N Roheryanto <wnroheryanto@hotmail.com>
2025-07-01 10:52:04 +0800

    Populate tool_x.cfg

--- btc_util.pl
+++ btc_util.pl
@@ -3,11 +3,17 @@ use strict;
 
 use File::Fetch;
 use IO::Uncompress::Unzip qw($UnzipError);
+use IO::File;
+use JSON::PP 'decode_json';
+use File::Spec::Functions qw(splitpath);
+use File::Path qw(mkpath);
+use File::Copy qw(copy);
 
 my $bundle_filename = "btc.zip";
-my $zip_bundle = "https://raw.githubusercontent.com/Bikin-Creative/Lineux-Toolchanger/btc_dev/$bundle_filename";
+my $src_api = "https://api.github.com/repos/Bikin-Creative/Lineux-Toolchanger/releases/latest";
 my $btc_install_folder = glob("~/btc");
 my $btc_staging_folder = "$btc_install_folder/config";
+my $btc_backup_folder = "$btc_install_folder/backup";
 my $btc_staging_variables = "$btc_staging_folder/btc_variables.cfg";
 my $btc_default_variables = "$btc_install_folder/default_variables.txt";
 my $btc_unzip_folder = "$btc_install_folder/unzip";
@@ -23,18 +29,112 @@ if (!-d $btc_cfg_folder) { mkdir ($btc_cfg_folder, 0775) or $!{EEXIST} or die "M
 symlink($btc_cfg_folder, $btc_staging_folder);
 &move_includes_frombtc; # Move includes from btc.cfg. For those using very early versions
 
-#&download_bundle;
+my ($dowhat, $latest_tag);
+my $tooltxt = "";
+my $decoded;
+&main_menu;
 
 
 
 
-&update_file; # Update btc_variables.cfg
+
+
+#&update_file; # Update btc_variables.cfg
 #use Data::Dumper; print Dumper $defval{$btc_default_variables}; exit;
 
+sub main_menu
+{ print "\nWelcome to Btc Utility\nWhat do you want to do?\n1. Install new\n2. Update\n3. Add tool\n4. Quit\n\nYour choice: ";
+	my $dowhat = <STDIN>;
+  chomp $dowhat;
+  if ($dowhat eq "1") { &doinstall; }
+  elsif ($dowhat eq "4") { exit; }
+}
+
+sub doinstall
+{ print "\nWhat do you want to install?\n1. Install Btc (Dockslide included)\n2. Install Dockslide (Not required if Btc is installed)\n3. Back\n4. Quit\n\nYour choice: ";
+	my $dowhat = <STDIN>;
+  chomp $dowhat;
+  if ($dowhat eq "1") { &installbtc; }
+  elsif ($dowhat eq "2") { &installdockslide; }
+  elsif ($dowhat eq "3") { &main_menu; }
+  elsif ($dowhat eq "4") { exit; }
+  else
+  { print "\nInvalied choice: $dowhat\n";
+  	### Do something TODO
+  	&doinstall;
+  }
+}
+
+sub installbtc
+{ print "\nHow many tools will you be using? Your choice: ";
+	my $toolnum = <STDIN>;
+  chomp $toolnum;
+  my @alltoolboards = ("H36", "EBB36", "SHT36", "Nitehawk", "Others");
+  my @toolboards = ();
+  my $toolboard;
+  for (my $i=1;$i<=$toolnum;$i++)
+  { print "\nWhat is toolboard $i?\n1. H36, 2. EBB36, 3. SHT36, 4. Nitehawk, 5. Others or not sure. Your choice: ";
+	  $toolboard = <STDIN>;
+    chomp $toolboard;
+    push @toolboards, $alltoolboards[$toolboard-1];
+    $tooltxt .= "Tool $i: $alltoolboards[$toolboard-1]\n";
+  }
+  &download_bundle;
+  &backupconfig;
+  &moveconfig;
+  &setuptoolboard(\@toolboards);
+	print "\nInstalling ...";
+  print "\nDone!\n$tooltxt\nNext step is to firmware restart and check for cfg errors.\n";
+}
+
+sub setuptoolboard
+{ my @toolboards = @{$_[0]};
+	open my $tooltemplate, "<", "$btc_install_folder/toolboard_template.txt";
+	my $template = do { local $/; <$tooltemplate> };
+	open my $pinjson, "<", "$btc_install_folder/sht36_pins.json";
+	my $json = do { local $/; <$pinjson> };
+  $decoded = decode_json($json);
+  $latest_tag = $decoded->{tag_name};
+	for (my $i=0;$i<scalar(@toolboards);$i++)
+	{ my $newtemplate = $template;
+		$newtemplate =~ s/<gcode_macro_variables>/[gcode_macro _Variables_t$i]/;
+		keys %$decoded; # reset the internal iterator so a prior each() doesn't affect the loop
+    while(my($k, $v) = each %$decoded) { $newtemplate =~ s/<$k>/$k: toolboard$i: $v/; }
+    open my $newcfg, ">", "$btc_install_folder/tmp/tool_$i.cfg";
+    print $newcfg $newtemplate;
+	}
+}
+
 sub download_bundle
-{ my $ff = File::Fetch->new(uri => $zip_bundle);
-  my $file = $ff->fetch(to => "$btc_install_folder") or die $ff->error;
-	unzip($bundle_filename, "$btc_unzip_folder");
+{ print "Downloading package...";
+	my $ff = File::Fetch->new(uri => $src_api);
+  my $fh = $ff->fetch(to => "$btc_install_folder") or die $ff->error;
+	open my $latest, "<", "$btc_install_folder/latest";
+	my $json = do { local $/; <$latest> };
+  my $decoded = decode_json($json);
+  $latest_tag = $decoded->{tag_name};
+  $latest_tag =~ s/^v//;
+  # If update available TODO
+
+  my $zip_bundle = $decoded->{zipball_url};
+  $ff = File::Fetch->new(uri => $zip_bundle);
+  $fh = $ff->fetch(to => "$btc_install_folder") or die $ff->error;
+  rename $fh, "$btc_install_folder/$latest_tag.zip";
+  unzip("$btc_install_folder/$latest_tag.zip", "$btc_unzip_folder");
+  unlink("$btc_install_folder/$latest_tag.zip");
+}
+
+sub backupconfig
+{ print "\nBacking up current configs...";
+	if (!-d "$btc_backup_folder/30062025") { mkdir ("$btc_backup_folder/30062025", 0775) or $!{EEXIST} or die "Make directory $btc_install_folder failed!!"; }
+	rename $btc_cfg_folder, "$btc_backup_folder/30062025/";
+	mkdir ($btc_cfg_folder, 0775);
+}
+
+sub moveconfig
+{ print "\nCopying new configs...";
+	my $zipfolder = "$btc_unzip_folder/Bikin-Creative-Lineux-Toolchanger-9d9acd0Klipper";
+	rename $zipfolder, "$btc_cfg_folder/";
 }
 
 sub move_includes_frombtc
@@ -107,4 +207,82 @@ sub read_entry
     }
   }
   return $key, \%values, @variables
+}
+
+# Unzip code by Daniel S. Sterling <sterling.daniel@gmail.com>
+# https://gist.github.com/eqhmcow/5389877
+sub unzip
+{ my ($file, $dest) = @_;
+
+  die 'Need a file argument' unless defined $file;
+  $dest = "." unless defined $dest;
+  $dest =~ s!/|\\$!!;
+
+  die "File argument is a directory: $file" if -d $file;
+
+  die "No such file: $file $!" unless -e $file;
+
+  my $u = IO::Uncompress::Unzip->new($file) or die "Cannot open $file: $UnzipError";
+
+  my $status;
+  my %dirs;
+  for ($status = 1; $status > 0; $status = $u->nextStream())
+  { # bail on error
+    last if $status < 0;
+
+    my $header = $u->getHeaderInfo();
+    my $stored_time = $header->{'Time'};
+    my (undef, $path, $name) = splitpath($header->{Name});
+    $path =~ s!/|\\$!!;
+    $name =~ s!/|\\$!!;
+
+    my $destdir = "/$dest/$path";
+    my $destfile = "$destdir/$name";
+    # https://cwe.mitre.org/data/definitions/37.html
+    # CWE-37: Path Traversal
+    die "unsafe $destfile" if $destfile =~ m!\Q..\E(/|\\)!;
+
+    # don't try to overwrite an extant file by creating a directory
+    if (-e $destdir and not -d $destdir) { die "Cannot create directory $destdir: File or path already exists.\nTry extracting to a different directory."; }
+
+    # skip if the entire path is just an extant directory
+    if (-d $destfile) { next; }
+
+    # ok let's make a directory for this zip archive entry
+    unless (-d $destdir)
+    { mkpath($destdir) or die "Couldn't mkdir $destdir: $!";
+
+      # we're done if the entire path is simply the directory we
+      # just created
+      if (-d $destfile)
+      { # this entry is probably for the directory itself, so store
+        # its mtime, because we have to touch all the dirs after
+        # creating all the files; otherwise as we process the archive,
+        # file creation will just reset each directory's mtime
+        $dirs{$destdir} = $stored_time;
+
+        next;
+      }
+    }
+
+    # ok we should have a valid file here we can extract
+    my $buff;
+    my $fh = IO::File->new($destfile, "w") or die "Couldn't write to $destfile: $!";
+
+    $fh->binmode();
+
+    while (($status = $u->read($buff)) > 0) { $fh->write($buff); }
+    $fh->close();
+    utime ($stored_time, $stored_time, $destfile) or die "Couldn't touch $destfile: $!";
+  }
+
+  die "Error processing $file: $UnzipError $!\n" if $status < 0 ;
+
+  # touch all the dirs that we created and that also had explicit directory
+  # entries in the archive
+  foreach my $dirpath (keys %dirs)
+  { my $stored_time = $dirs{$dirpath};
+    utime ($stored_time, $stored_time, $dirpath) or die "Couldn't touch directory $dirpath: $!";
+  }
+  return;
 }
--- install.pl
+++ install.pl
@@ -6,7 +6,10 @@ $btc_install_folder = glob("~/btc");
 $util_file = "btc_util.pl";
 $btc_source = "https://raw.githubusercontent.com/Bikin-Creative/Lineux-Toolchanger/btc_dev/$util_file";
 
-if (!-d $btc_install_folder) { mkdir ($btc_install_folder, 0775) or $!{EEXIST} or die "Make directory $btc_install_folder failed!!"; }
+if (!-d $btc_install_folder)
+{ mkdir ($btc_install_folder, 0775) or $!{EEXIST} or die "Make directory $btc_install_folder failed!!";
+	mkdir ("$btc_install_folder/backup", 0775) or $!{EEXIST} or die "Make directory $btc_install_folder failed!!";
+}
 $ff = File::Fetch->new(uri => $btc_source);
 $file = $ff->fetch(to => "$btc_install_folder") or die $ff->error;
 chmod 0755, "$btc_install_folder/$util_file";
